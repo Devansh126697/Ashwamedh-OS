195f38e48bdf2cdb3a348e843b42e9a801141165
Devansh126697 <devanshp6697@gmail.com>
2025-05-17 19:22:29 +0530

    Final Bootloader!

--- phase1/bootloader.asm
+++ phase1/bootloader.asm
@@ -1,21 +1,38 @@
 [org 0x7C00]
 
 start:
-    ; Print a basic message using BIOS interrupt
-    mov si, message
-    mov ah, 0x0E
-
-print_loop:
-    lodsb
-    cmp al, 0
-    je hang
+    ; Clear the screen (black background)
+    mov ax, 0x0600
+    mov bh, 0x0E               ; Yellow on black
+    mov cx, 0x0000
+    mov dx, 0x184F
     int 0x10
-    jmp print_loop
+
+    ; Set up VGA memory
+    mov ax, 0xB800
+    mov es, ax
+    mov di, (12 * 80 + 25) * 2
+
+    ; Load message and print
+    mov si, message
+    call print_string
 
 hang:
     jmp $
 
-message db "Ashwamedh OS Test Boot", 0
+print_string:
+print_loop:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0E
+    stosw
+    jmp print_loop
+
+done:
+    ret
+
+message db ">>>  ASHWAMEDH OS IS RISING  <<<", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
